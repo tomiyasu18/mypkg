a20f7d11f504f0d5b6cd74f1fd0a32ce9417ce7d
Hiromu Masuda <s21C1109EW@s.chibakoudai.jp>
2022-12-01 12:04:36 +0900

    Initial Commit

---
+++ mypkg/talker.py
@@ -0,0 +1,17 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+def cb(request, response):
+    if request.name == "増田大夢":
+        response.age = 44
+    else:
+        response.age = 255
+
+    return response
+
+rclpy.init()
+node = Node("talker")
+srv = node.create_service(Query,"query", cb)
+rclpy.spin(node)
+
